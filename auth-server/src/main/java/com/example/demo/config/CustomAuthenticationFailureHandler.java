6a251a17e5906ffac2f10d1f842468ab97364ebc
Chatterjee.Avijit <avijit.liberty@gmail.com>
2018-04-07 11:49:42 -0400

    Minor fix to return authentication exception message

--- auth-server/src/main/java/com/example/demo/config/CustomAuthenticationFailureHandler.java
+++ auth-server/src/main/java/com/example/demo/config/CustomAuthenticationFailureHandler.java
@@ -1,19 +1,24 @@
 package com.example.demo.config;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.LocaleResolver;
 
 @Component("authenticationFailureHandler")
 public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler  {
 
+	
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException exception) throws IOException, ServletException {
@@ -21,6 +26,7 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
 		// Custom failure code here
         setDefaultFailureUrl("/login?error=true");
         super.onAuthenticationFailure(request, response, exception);
-        request.getSession().setAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, exception);
+        
+        request.getSession().setAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, exception.getMessage());
 	}	 
 }
