e86fb005189e89d8502c1cd31f1407d36975846e
avijitliberty <avijit.liberty@gmail.com>
2018-04-08 23:38:58 -0400

    Added eureka-server Junit

--- eureka-server/src/test/java/com/example/demo/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/example/demo/EurekaServerApplicationTests.java
@@ -1,16 +1,37 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = EurekaServerApplication.class)
 public class EurekaServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Value("${local.server.port}")
+    private int port = 9999;
+
+    @Test
+    public void catalogLoads() {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = new TestRestTemplate().getForEntity("http://localhost:" + port + "/eureka/apps", Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+
+    @Test
+    public void adminLoads() {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = new TestRestTemplate().getForEntity("http://localhost:" + port + "/env", Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
 
 }
