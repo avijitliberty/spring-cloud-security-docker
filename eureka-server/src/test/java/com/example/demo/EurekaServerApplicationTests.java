89da9f2282ea553bbf221910ddc38b093df4af18
Chatterjee.Avijit <avijit.liberty@gmail.com>
2018-05-21 14:54:06 -0400

    Removed dependency on config-server and mysql DB for running JUnit tests

--- eureka-server/src/test/java/com/example/demo/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/example/demo/EurekaServerApplicationTests.java
@@ -11,10 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = EurekaServerApplication.class)
+@ActiveProfiles("test")
 public class EurekaServerApplicationTests {
 
     @Value("${local.server.port}")
